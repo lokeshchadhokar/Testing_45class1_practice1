e20e80b638eebc0d82ec7809c0bd197a52b8f1f4
lokeshchadhokar <133833057+lokeshchadhokar@users.noreply.github.com>
2023-05-24 06:54:37 +0530

    Add files via upload

---
+++ chechking.py
@@ -0,0 +1,7 @@
+#my name is lokesh
+#follow the procces
+#addition of two number
+a = 20
+b = 30
+print("additon of a,b is :-",a+b)
+# git status is use to save the current modification data in our local folder/commit/storege etc
